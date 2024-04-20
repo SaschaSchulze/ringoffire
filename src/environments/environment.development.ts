678c9645f7cb19b044dda47dd8eea8116dbce7ea
Sascha Schulze <sascha-schulze1@web.de>
2024-04-20 20:15:49 +0200

    test

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,10 @@
-export const environment = {};
+export const environment = {
+    firebaseConfig: {
+        apiKey: 'AIzaSyAr6oj-d4eT2kcH-GQFONWSABGud-OlSZM',
+        authDomain: 'ring-of-fire-fd256.firebaseapp.com',
+        projectId: 'ring-of-fire-fd256',
+        storageBucket: 'ring-of-fire-fd256.appspot.com',
+        messagingSenderId: '308837261938',
+        appId: '1:308837261938:web:fc237788915147f13ad8eb',
+      },
+};
